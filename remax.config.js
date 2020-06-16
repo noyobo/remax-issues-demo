8c636221da2f5453b3bade08adf6d29219b061c8
农有宝 <nongyb@weipaitang.com>
2020-06-16 13:40:42 +0800

    平台属性demo演示

--- remax.config.js
+++ remax.config.js
@@ -1,4 +1,24 @@
+const node = require('@remax/wechat/node').default;
+
+const defaultHostComponents = node().hostComponents;
+
+defaultHostComponents.set('view', {
+  ...defaultHostComponents.get('view'),
+  props: [...defaultHostComponents.get('view').props, 'catchtouchmove'],
+});
+
 module.exports = {
-  one: true,
-  output: 'dist/' + process.env.REMAX_PLATFORM
+  one: false,
+  output: 'dist/' + process.env.REMAX_PLATFORM,
+  plugins: [
+    {
+      hostComponents: defaultHostComponents,
+      // processProps: ({ componentName, props, additional, node }) => {
+      //   if (componentName === 'view') {
+      //     return [...props, 'catchtouchmove'];
+      //   }
+      //   return props;
+      // },
+    },
+  ],
 };
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,33 +1,24 @@
 import * as React from 'react';
-import { View, Text, Image } from 'remax/one';
+import { View } from '@remax/wechat';
 import styles from './index.css';
-import Link from '../../../components/Link';
-import HOC from './Hoc.js';
 
 class Home extends React.Component {
-  onHide() {
-    console.log('hide');
-  }
-  onReady() {
-    console.log('ready');
-  }
   render() {
+    const restProps = {
+      'wechat-catchtap': () => {
+        console.log('catch tap');
+      },
+      'wechat-bindtap': console.log,
+    };
     return (
-      <View className={styles.app}>
-        <Link>Link 标签</Link>
-        <View className={styles.header}>
-          <Image
-            src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ"
-            className={styles.logo}
-            alt="logo"
-          />
-          <View className={styles.text}>
-            编辑 <Text className={styles.path}>src/pages/index/index.js</Text>{' '}
-            开始
-          </View>
-        </View>
+      <View
+        className={styles.app}
+        {...restProps}
+        wechat-catchtouchmove={console.log}
+      >
+        绑定事件
       </View>
     );
   }
 }
-export default HOC(Home);
+export default Home;
