1e99b2d79a62fc10d8a72b0a7847415e25d00dc6
noyobo <noyobo@gmail.com>
2020-12-03 14:20:19 +0800

    demo

--- src/app.config.js
+++ src/app.config.js
@@ -1,30 +1,30 @@
 const pages = ['pages/index/index'];
 
 module.exports.ali = {
-  pages,
-  window: {
-    defaultTitle: 'Remax Ali Template',
-    titleBarColor: '#282c34',
-  },
+    pages,
+    window: {
+        defaultTitle: 'Remax Ali Template',
+        titleBarColor: '#282c34',
+    },
 };
 
 module.exports.wechat = {
-  pages,
-  window: {
-    navigationBarTitleText: 'Remax Wechat Template',
-    navigationBarBackgroundColor: '#282c34',
-  },
+    pages,
+    window: {
+        navigationBarTitleText: 'Remax Wechat Template',
+        navigationBarBackgroundColor: '#282c34',
+    },
 };
 
 module.exports.toutiao = {
-  pages,
-  window: {
-    navigationBarTitleText: 'Remax Toutiao Template',
-    navigationBarBackgroundColor: '#282c34',
-  },
+    pages,
+    window: {
+        navigationBarTitleText: 'Remax Toutiao Template',
+        navigationBarBackgroundColor: '#282c34',
+    },
 };
 
 module.exports.web = {
-  pages,
-  title: 'Remax Web Template',
+    pages,
+    title: 'Remax Web Template',
 };
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,33 +1,23 @@
 import * as React from 'react';
-import { View, Text, Image } from 'remax/one';
-import styles from './index.css';
-import Link from '../../../components/Link';
-import HOC from './Hoc.js';
+import { View, Input } from 'remax/wechat';
 
 class Home extends React.Component {
-  onHide() {
-    console.log('hide');
-  }
-  onReady() {
-    console.log('ready');
-  }
-  render() {
-    return (
-      <View className={styles.app}>
-        <Link>Link 标签</Link>
-        <View className={styles.header}>
-          <Image
-            src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ"
-            className={styles.logo}
-            alt="logo"
-          />
-          <View className={styles.text}>
-            编辑 <Text className={styles.path}>src/pages/index/index.js</Text>{' '}
-            开始
-          </View>
-        </View>
-      </View>
-    );
-  }
+    onHide() {
+        console.log('hide');
+    }
+    onReady() {
+        console.log('ready');
+    }
+    render() {
+        return (
+            <View>
+                <Input
+                    placeholder="搜索属性"
+                    wechat-confirm-type="search"
+                    wechat-bindconfirm={console.log}
+                />
+            </View>
+        );
+    }
 }
-export default HOC(Home);
+export default Home;
